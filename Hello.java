44c2c11c45b0d3b36366af523c0c4a957c48950b
Joan Njuguna <njugunajoan35@gmail.com>
2020-06-23 19:46:58 +0300

    Intro to Java

---
+++ Hello.java
@@ -0,0 +1,94 @@
+package com.example.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        /**
+         * This is a simple intro to Variables and Arithmmetic Operators
+         * in Java.
+         */
+//        System.out.println("Hello World");
+//        System.out.print("Hello World");
+        // TODO Finish variables
+
+//        // Integers store whole numbers and negative numbers
+//        int number = -5;
+//        System.out.println(number);
+//
+//        // Store whole numbers too
+//        long myLong =  5;
+//        System.out.println(myLong);
+//
+//        // Store larger decimals and occupy more space
+//        double myDouble = 4.5;
+//        System.out.println(myDouble);
+//
+//        // Store smaller decimals
+//        float myFloat = (float) 4.5;
+//        System.out.println(myFloat);
+//
+//        // Characters store special single characters and unicodes
+//        char myChar = 'J';
+//        System.out.println(myChar);
+//
+//        char myChar2 = '\u00A9';
+//        System.out.println(myChar2);
+//
+//        // Strings are for double(multiple) characters
+//
+//        String name = "Joan";
+//        System.out.println(name);
+//
+//        // Boolean only store true and false values
+//        boolean myBoolean = false;
+//        System.out.println(myBoolean);
+
+        //Arithmetic Operators
+//        int a = 6;
+//        int b = 2;
+
+        //addition
+//        int answer =  a + b;
+//        System.out.println(answer);
+
+        //subtraction
+        //int answer =  a - b;
+        //System.out.println(answer);
+
+        //multiplication
+//        int answer =  a * b;
+//        System.out.println(answer);
+
+        //division
+        // (never divide by b = 0 - it will cause an exception. Alternatively catch the exception.)
+//        int answer =  a / b;
+//        System.out.println(answer);
+
+        // (to have the answer in a floating point e.g 2.5, cast your answer in a double e.g)
+//        int c = 5;
+//        int d = 2;
+//
+//        double result = (double) c / d;
+//        System.out.println(result);
+
+        //modulus (gives the remainder of two numbers)
+//        int answer = a % b;
+//        System.out.println(answer);
+
+        // String Concatenation
+//
+//        String firstName = "Joan ";
+//        String lastName = "Njuguna";
+//
+//        String fullName = firstName + lastName;
+//
+//        System.out.println(fullName);
+
+
+
+
+
+//
+
+    }
+}
