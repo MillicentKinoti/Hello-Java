8b65bfea9e4b5d8f54dc6aee115641991e92d86e
Joan Njuguna <njugunajoan35@gmail.com>
2020-06-23 19:48:16 +0300

    Java Operators and Conditional Statements

---
+++ Main.java
@@ -0,0 +1,140 @@
+package com.example.operators;
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * This will be a project to showcase
+         * Relational and Logical Operators and
+         * Conditional Statements
+         */
+
+        // But first
+        // In regards to previous project( Variables & Arithmetic Operators)
+        // This is how to simplify them e.g
+
+        //int a = 5;
+
+        //instead of this
+        // a =  a + 2;
+        // you could do
+        // a += 2;
+        // applies to also
+        // a -= 2;
+        // a *= 2;
+        // a /= 2;
+
+
+        // Before I forget, this is how you could simply increment or decrement a variable by 1
+        // instead of
+//
+//        a =  a + 1;  a = a - 1; // or
+//
+//        a =+ 1; a -= 1;
+
+        // you could do
+
+        // a++; // or
+
+        // a--;
+
+        //System.out.println(a);
+
+        /**
+         * Onto Relational and Logical Operators
+         */
+
+        // Comparison/Relational Operators
+
+        int a =  1;
+        int b = 2;
+
+        // to assign answer to true or false store it in a boolean
+        // greater than
+        //boolean answer = a > b;
+
+        //less than
+        //boolean answer = a < b;
+
+        //equals to (to check equality use ==)
+        //boolean answer = a == b;
+
+        // greater than or equal to
+//        boolean answer = a >= b;
+
+        //less than or equal to
+        //boolean answer = a <= b;
+
+        //is not equal to
+        //boolean answer = a != b;
+        //System.out.println(answer);
+
+        /**
+         * Logical Operators
+         */
+
+        // OR (||) operators, if one condition is true print true
+        //boolean answer = a == 5 || b == 2;
+
+        // AND  (&&) operators, if both conditions are true print true
+        //boolean answer = a == 5 && b == 2;
+        //System.out.println(answer);
+
+        /**
+         * Conditional Statements
+         */
+
+        // if statements
+
+        boolean answer = a > 3;
+
+//        if (answer) {
+//            System.out.println("A is greater than 3");
+//        }
+//        // else statements
+//        else {
+//            System.out.println("A is less than 3");
+//        }
+
+        // else if statements
+
+//        if ( a > 0 ) {
+//            System.out.println("A is Positive");
+//        } else if ( a < 0 ) {
+//            System.out.println("A is Negative");
+//        }
+//        else {
+//            System.out.println("A is Zero");
+//        }
+
+        /**
+         * Switch statements
+         */
+
+        // we have a break statement in every case statement to avoid it from breaking the switch statement
+        // the 'default' case statement is printed out if none of the conditions are true
+//
+//        switch (a) {
+//            case 1:
+//                System.out.println("A is One");
+//                break;
+//            case 2:
+//                System.out.println("A is Two");
+//                break;
+//            case 3:
+//                System.out.println("A is Three");
+//                break;
+//            default:
+//                System.out.println("A is not One, Two or Three");
+//                break;
+//        }
+//
+//
+//        System.out.println("Continue...");
+
+        /**
+         * The End
+         */
+
+
+    }
+}
